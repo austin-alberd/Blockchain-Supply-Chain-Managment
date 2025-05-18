6cb604852d6aedcaedcbfa6e6571c9477243cb97
Austin Alberd <austinalberd2005@gmail.com>
2025-05-17 20:35:06 -0500

    basic API outline

---
+++ server/api/api.go
@@ -0,0 +1,111 @@
+package api
+
+import (
+	_ "database/sql"
+	"fmt"
+	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+// A struct that holds all of the data of the action
+type DataContainer struct {
+	ItemName      string // Name of the item effected
+	Action        string // What was done to the item ("add, remove, update, etc.")
+	DateTimeStamp string // Time and Date stamp of the action
+	Comments      string // Comments about the action
+}
+
+// A struct that hold the block
+type Block struct {
+	Hash     string        // The hash of the block
+	PrevHash string        // The hash of the previous block
+	Data     DataContainer // Data Stored in the Block
+}
+
+// Blockchain Helper Functions
+
+/*
+	Name:addBlock
+
+Purpose:Adds a block to the blockchain
+Returns: true if block is added | false if there is an error
+*/
+func addBlock() bool {
+	return false
+}
+
+/*
+	Name:validateChain
+
+Purpose:Validates the blockchain
+Returns:true if the chain is valid | false if the chain is invalid
+*/
+func validateChain() bool {
+	return false
+}
+
+// Inventory System API Endpoint Functions
+
+/*
+	Name:newItem
+
+Purpose:HTTP function to add the new item
+Returns:Nothing
+*/
+func newItem(w http.ResponseWriter, req *http.Request) {
+	http.Error(w, "Not Implemented", http.StatusNotImplemented)
+}
+
+/*
+	Name:getItem
+
+Purpose:HTTP function to get an item from the database
+Returns:
+*/
+func getItem(w http.ResponseWriter, req *http.Request) {
+	http.Error(w, "Not Implemented", http.StatusNotImplemented)
+}
+
+/*
+	Name:removeItem
+
+Purpose:HTTP function to remove an item from the database
+Returns:
+*/
+func removeItem(w http.ResponseWriter, req *http.Request) {
+	http.Error(w, "Not Implemented", http.StatusNotImplemented)
+}
+
+/*
+	Name:updateItem
+
+Purpose:HTTP function to update an item in the inventory
+Returns:
+*/
+func updateItem(w http.ResponseWriter, req *http.Request) {
+	http.Error(w, "Not Implemented", http.StatusNotImplemented)
+}
+
+/*
+	Name:ExposeAPI
+
+Purpose: Exposes the API to the user
+Returns:
+*/
+func ExposeAPI() {
+	http.HandleFunc("/api/v1/createItem", newItem)
+	http.HandleFunc("/api/v1/getItem", getItem)
+
+	http.HandleFunc("/api/v1/removeItem", removeItem)
+	http.HandleFunc("/api/v1/updateItem", updateItem)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		http.Error(w, "API ROOT PAGE", http.StatusTeapot)
+	})
+
+	err := http.ListenAndServe("127.0.0.1:8080", nil)
+	if err != nil {
+		fmt.Println("💀[FATAL ERROR]: Could not start server", err)
+	}
+}
--- server/main.go
+++ server/main.go
@@ -3,35 +3,26 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"main/api"
+	"runtime"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 var connectionString = "./prod.db"
 
-type DataContainer struct {
-	StockNumber     string // The identifier number of the item
-	ItemName        string // The name of the item
-	ItemDescription string // The item's description
-	ItemsInStock    int    // How many are in stock
-}
-
-type Block struct {
-	Hash     string        // The hash of the block
-	PrevHash string        // The hash of the previous block
-	Data     DataContainer // Data Stored in the Block
-}
-
 func main() {
 
-	// Set up the database.
+	// Connect to the database
 	db, err := sql.Open("sqlite3", connectionString)
 
+	// Can not connect to the database. UhOh
 	if err != nil {
 		fmt.Println("💀 [FATAL ERROR]: Could not connect to db", err)
+		runtime.Goexit()
 	}
 
-	defer db.Close()
+	// SQL Statement to create the database needed to store the blockchain data.
 	sqlStatement := `
 		CREATE TABLE IF NOT EXISTS blockchain (
 			hash LONGTEXT NOT NULL PRIMARY KEY,
@@ -40,13 +31,16 @@ func main() {
 		)
 	`
 
+	// Execute the statement and handle any errors that result from it.
 	_, err = db.Exec(sqlStatement)
 	if err != nil {
 		fmt.Println("💀[FATAL ERROR]: Could not create blockchain table \n", err)
+		runtime.Goexit()
 	} else {
-		fmt.Println("✅[SUCCESS]: Created blockchain table")
+		fmt.Println("✅[SUCCESS]: Blockchain Table is Ready")
 	}
 
+	// SQL Statement to create the inventory table.
 	sqlStatement = `
 		CREATE TABLE IF NOT EXISTS inventory (
 			itemName LONGTEXT NOT NULL PRIMARY KEY,
@@ -56,42 +50,18 @@ func main() {
 		)
 	`
 
+	// Execute the statement and handle any errors that result from it.
 	_, err = db.Exec(sqlStatement)
 	if err != nil {
 		fmt.Println("💀[FATAL ERROR]: Could not create inventory table \n", err)
+		runtime.Goexit()
 	} else {
-		fmt.Println("✅[SUCCESS]: Created inventory table")
+		fmt.Println("✅[SUCCESS]: Inventory Table is Ready")
 	}
 
+	// Success Message
 	fmt.Println("✅[SUCCESS]: Completed Initial Setup \n")
 
-	//Variable initializations
-	var userMenuChoice int // A holder for the user's choice in the menu
-
-	for {
-		// Print out the menu
-		fmt.Println("1) Expose API")
-		fmt.Println("2) Validate Block Chain")
-		fmt.Println("3) Manual Insert Block")
-		fmt.Println("4) Exit")
-		fmt.Println("CHOICE (1-4):")
-		fmt.Scan(&userMenuChoice)
-
-		switch userMenuChoice {
-		case 1:
-			fmt.Println("\nExpose API")
-		case 2:
-			fmt.Println("\nValidate Block Chain")
-		case 3:
-			fmt.Println("\nManual Insert Block")
-		case 4:
-			break
-		}
-
-		if userMenuChoice == 4 {
-			// Its messy I know. Womp Womp
-			break
-		}
-	}
-
+	// Expose the API Endpoint
+	api.ExposeAPI()
 }
